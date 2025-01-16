fef44e11bda2ab0cab2043181a07d7c36b95cda8
William Wiik <william.wiik@hotmail.se>
2025-01-16 17:04:32 +0100

    final commit?

---
+++ combine_data.R
@@ -0,0 +1,79 @@
+
+library(dplyr)
+library(stringr)
+library(tidytext)
+library(tokenizers)
+library(sentimentr)
+library(stringi)
+library(ggplot2)
+
+# From file premierleague.R
+# Webscraped "https://www.premierleague.com/results" to get all the match reports
+list_report <- readRDS("RDS_files/list_report.RDS")
+
+# From file fotmob.R
+# Used Fotmobs API to get player performance of the same matches as in the match reports
+list_stats <- readRDS("RDS_files/list_stats.RDS")
+
+
+
+# The two datsets uses different names on football teams
+# thats why we have a translator
+team_translator <- readRDS("RDS_files/team_translator.RDS") %>% as.data.frame()
+
+translate <- function(team) {
+  team_translator[team_translator$PL == team, "fotmob"]
+}
+
+
+# Merging the two datasets list_report and list_stats
+for(i in 1:length(list_stats)) {
+  
+  hteam_stats <- list_stats[[i]]$hteam
+  ateam_stats <- list_stats[[i]]$ateam
+  
+  
+  all_hteam_ateam_report <- as.vector(unname(unlist(sapply(list_report,
+                                      function(x){
+                                        translate_hteam <- translate(x$hteam)
+                                        translate_ateam <- translate(x$ateam)
+                                        str_c(translate_hteam,translate_ateam)
+                                      }))))
+  
+  index_report <- which(all_hteam_ateam_report == str_c(hteam_stats, ateam_stats))
+  
+  list_stats[[i]]$report <- list_report[[index_report]]$report
+}
+
+
+saveRDS(list_stats, "RDS_files/combined_stats_report_list.RDS")
+
+
+
+
+# Data section, ggplot histogram
+players_rating <- as.numeric(unlist(sapply(list_stats, function(x){
+  c(x$home_players$FotMob.rating, x$away_players$FotMob.rating)
+} )))
+
+
+players_rating <- players_rating[!players_rating == 0]
+
+
+
+# Assuming players_rating is a numeric vector
+ggplot(data = data.frame(players_rating), aes(x = players_rating)) +
+  geom_histogram(binwidth = 0.5, fill = "lightblue", color = "black") +
+  labs(title = "Histogram of FotMob Ratings", x = "FotMob Rating", y = "Frequency") +
+  theme_minimal() +
+  theme(
+    text = element_text(size = 13),      # Increase text size
+    axis.title = element_text(size = 15), # Increase axis title size
+    axis.text = element_text(size = 12),  # Increase axis number size
+    plot.title = element_text(size = 17)  # Increase plot title size
+  )
+
+
+ggsave("player_ratings_histogram.png", width = 5, height = 3.5, dpi = 300)
+
+
--- gather_data.R
+++ gather_data.R
@@ -1,7 +1,16 @@
 
 library(dplyr)
 library(httr)
+library(jsonlite)
+library(RSelenium)
+library(wdman)
+library(netstat)
+library(dplyr)
+library(stringr)
+library(rvest)
 
+
+# FOTMOB API ===================================================================
 
 xfmreq <- "eyJib2R5Ijp7InVybCI6Ii9hcGkvbWF0Y2hNZWRpYT9tYXRjaElkPTQ1MDY0NDUmY2NvZGUzPVNXRSIsImNvZGUiOjE3MzQzNTAzNDIwMDEsImZvbyI6IjBjMjkwYzM4OSJ9LCJzaWduYXR1cmUiOiJFMDA5MzlGQkQyREEzOTAzQURBMjQ3MjMxNTBENUU5OCJ9"
 
@@ -178,7 +187,126 @@ for(i in 1:nrow(matches)) {
   
 }
 
-saveRDS(list_stats, "list_stats.RDS")
+saveRDS(list_stats, "RDS_files/list_stats.RDS")
+
+
+
+
+
+
+
+
+
+# www.premierleague.com WEB SCRAPE =============================================
+
+url <- "https://www.premierleague.com/results"
+
+
+
+remote_driver <- rsDriver(browser = "firefox",chromever = NULL,
+                          verbose = F, port = free_port())
+remDrv <- remote_driver$client
+remDrv$maxWindowSize()
+
+remDrv$navigate(url)
+
+html_content <- remDrv$getPageSource()[[1]]
+html_parsed <- read_html(html_content)
+# Select elements with the data-href attribute
+matchurls <- html_parsed %>%  html_nodes('[data-href]') %>%  html_attr('data-href')
+
+
+
+list_report <- list()
+
+for(i in 1:length(matchurls)) {
+  
+  print(i)
+  html_content <- read_html(str_c("https:",matchurls[i]))
+  match_report <- html_content %>% html_nodes('.article__body p') %>% html_text() 
+  full_report <- paste(match_report, collapse = " ")
+  
+  
+  teamnames <- html_content %>% html_nodes('.mc-summary__team-name') %>% html_text() 
+  teamnames <- teamnames[c(T,F,T,F)]
+  
+  list_report <- append(list_report, list(list(report = full_report,
+                                               hteam = teamnames[1],
+                                               ateam = teamnames[2])))
+  
+  names(list_report)[length(list_report)] <- str_c(teamnames[1]," vs ", teamnames[2])
+  
+}
+saveRDS(list_report, "RDS_files/list_report.RDS")
+
+
+remote_driver$server$stop()
+
+
+
+
+
+
+
+
+
+
+# MERGE TEAMNAMES ==============================================================
+
+
+list_report <- readRDS("RDS_files/list_report.RDS")
+list_stats <- readRDS("RDS_files/list_stats.RDS")
+
+
+fotmob_names <- unique(as.vector(unname(unlist(sapply(list_stats,
+                                                      function(x) c(x$hteam,x$ateam))))))
+
+PL_names <- unique(as.vector(unname(unlist(sapply(list_report,
+                                                  function(x) c(x$hteam,x$ateam))))))
+
+mat <- matrix(NA, length(fotmob_names), 3)
+colnames(mat) <- c("fotmob","PL","valid")
+
+
+
+fotmob_names <- fotmob_names %>%  as.data.frame()
+colnames(fotmob_names) <- "team"
+
+
+PL_names <- PL_names %>% as.data.frame() 
+colnames(PL_names) <- "team"
+
+
+i <- 1
+for(team in fotmob_names$team) {
+  
+  index_team_PL <- which.min(lapply(PL_names, function(x) stringdist::stringdist(team, x, method = "jw")) %>% unlist())
+  dist_PL <- (lapply(PL_names, function(x) stringdist::stringdist(team, x, method = "jw")) %>% unlist())[index_team_PL]
+  
+  
+  
+  index_team <- index_team_PL
+  dist <- dist_PL
+  
+  
+  mat[i,1] <- team
+  mat[i,2] <- PL_names$team[index_team]
+  mat[i,3] <- ifelse(dist < 0.1, TRUE, FALSE)
+  
+  i <- i + 1
+}
+
+mat[20,2] <- "Spurs"
+mat[18,2] <- "Man City"
+
+
+saveRDS(mat[,1:2],"RDS_files/team_translator.RDS")
+
+
+
+
+
+
 
 
 
